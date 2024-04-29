061600a3f0129643436f38ebc56515191e17594f
Obemik <misaperepecaj@gmail.com>
2024-04-29 19:41:55 +0300

    Home ex1 done

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-    int num;
-    int sum = 0;
-    do {
-        cout << "Enter num: ";
-        cin >> num;
-        sum += num;
-    } while (num != 0);
+    int num1, sum = 0;
+    cout << "Enter num: ";
+    cin >> num1;
+    int current = num1;
+    while (current <= 500) {
+        sum += current;
+        current++;
+    }
     cout << "Sum: " << sum;
     return 0;
 }
